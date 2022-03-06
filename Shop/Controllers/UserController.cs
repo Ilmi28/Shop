7828861813c2391336e633598851d7ba388a7932
ilmialiev <ilmialiev28@gmail.com>
2022-03-06 19:13:07 +0100

    Add some files

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ILogger<UserController> _logger;
+        public UserController(UserManager<AppUser> userManager, ILogger<UserController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = user.Name,
+                    Email = user.Email,
+                    PasswordHash = user.Password
+                };
+                IdentityResult result = await _userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    _logger.LogError("Cannot create user with name {name} and email {}", user.Name, user.Email);
+                }
+            }
+            return View(user);
+        }
+    }
+}
